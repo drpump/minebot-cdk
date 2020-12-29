007b83d589240137c6d1b4e0a6d12c5d996c9871
Andrew Berry <andyb@deontik.com>
2020-12-30 10:41:14 +1100

    Refactored to allow creation of multiple tasks/services

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+import os
 
 from aws_cdk import core
 
 from minebot.minebot_stack import MinebotStack
 
-
 app = core.App()
-env_sydney = core.Environment(account="964583318248", region="ap-southeast-2")
+env_sydney = core.Environment(account=os.getenv("AWS_ACCOUNT", "964583318248"), 
+                              region=os.getenv("AWS_REGION", "ap-southeast-2"))
 MinebotStack(app, "minebot", env=env_sydney)
 
 app.synth()
--- minebot/minebot_stack.py
+++ minebot/minebot_stack.py
@@ -12,78 +12,112 @@ class MinebotStack(core.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # setup networking using default VPC
-        mc_vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)
-        mc_sg = ec2.SecurityGroup(self, 'minecraft-sg', 
-            vpc=mc_vpc,
-            allow_all_outbound=True,
-            description='minecraft security group'
-        )
-        mc_sg.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(25565), 'Allow minecraft from anywhere')
-        efs_sg = ec2.SecurityGroup(self, 'efs-sg',
-            vpc=mc_vpc,
+        self.vpc = ec2.Vpc.from_lookup(self, "VPC", is_default=True)
+        self.init_sec_groups()
+
+        # create cluster to hold instances (with Fargate this is more-or-less meaningless)
+        self.cluster = ecs.Cluster(self, "MinecraftCluster", vpc=self.vpc)
+
+        operator = 'Akemos_with_no_Q'
+        name = 'cats-in-bread'
+        guild = '786042802630950984'
+        self.create_service(name, guild, operator, self.cluster)
+
+
+    #
+    # Create security groups required for servers and an ssh utility container
+    #
+    def init_sec_groups(self):
+        self.efs_sg = ec2.SecurityGroup(self, 'efs-sg',
+            vpc=self.vpc,
             description='EFS security group'
         )
-        efs_sg.add_ingress_rule(mc_sg, ec2.Port.tcp(2049), 'Allow EFS clients')
-        mc_sg.add_ingress_rule(efs_sg, ec2.Port.tcp(2049), 'Allow EFS access')
+        self.minecraft_sg = self.efs_sec_group('minecraft', 25565)
+        self.ssh_sg = self.efs_sec_group('ssh', 22)
+        return
 
-        # create an EFS filesystem
-        fs = efs.FileSystem(self, 'minecraft-fs', 
-                            vpc=mc_vpc,
-                            performance_mode=efs.PerformanceMode.GENERAL_PURPOSE,
-                            throughput_mode=efs.ThroughputMode.BURSTING,
-                            security_group=efs_sg
+    #
+    # Create a security group for access to specified port and efs filesystems
+    #
+    def efs_sec_group(self, name, port):
+        sec_group = ec2.SecurityGroup(self, name + '-sg', 
+            vpc=self.vpc,
+            allow_all_outbound=True,
+            description=name + ' security group'
         )
-        fs.add_access_point("cats-in-bread", path="/")
+        sec_group.add_ingress_rule(ec2.Peer.any_ipv4(), ec2.Port.tcp(port), name + ' from anywhere')
 
-        # define an ECS volume for this filesystem
-        mc_volume = ecs.Volume(
-            name="cats-in-bread",
-            efs_volume_configuration=ecs.EfsVolumeConfiguration(
-                file_system_id=fs.file_system_id #, 
-                # root_directory="/cats_in_bread"
-            )
-        )
+        self.efs_sg.add_ingress_rule(sec_group, ec2.Port.tcp(2049), 'Allow clients from ' + name)
+        sec_group.add_ingress_rule(self.efs_sg, ec2.Port.tcp(2049), 'Allow efs access for ' + name)
 
-        # create an ECS task
-        minecraft = ecs.FargateTaskDefinition(self, "minecraft", 
+        return sec_group
+
+    #
+    # Create a service with zero instances for the identified task (we'll start it on demand)
+    #
+    # Service will be tagged with the guild id so we can find it later
+    #
+    def create_service(self, name, guild, operator, cluster):
+        service = ecs.FargateService(self, name + "-service", 
+                                    cluster=self.cluster, 
+                                    task_definition=self.create_task(name, guild, operator),
+                                    assign_public_ip=True,
+                                    desired_count=0,
+                                    security_group=self.minecraft_sg,
+                                    platform_version=ecs.FargatePlatformVersion.VERSION1_4)
+        core.Tags.of(service).add("guild", guild)
+        return service
+
+    def create_task(self, name, guild, operator):
+        # define an ECS task
+        task = ecs.FargateTaskDefinition(self, name, 
             cpu=1024, 
             memory_limit_mib=2048,
-            volumes=[mc_volume]
+            volumes=[self.create_efs_volume(name)]
         )
+        core.Tags.of(task).add("guild", guild)
+        self.create_container(name, task, operator)
+        return task
 
-        # add the minecraft container
-        mc_container = minecraft.add_container(
-            "minecraft",
+    def create_container(self, name, task, operator):
+        # define the minecraft container
+        container = task.add_container(
+            name,
             image=ecs.ContainerImage.from_registry("itzg/minecraft-server"),
             essential=True,
             environment={
                 "EULA": "TRUE", 
-                "OPS": "Akemos_with_no_Q",
+                "OPS": operator,
                 "ALLOW_NETHER": "true",
                 "ENABLE_COMMAND_BLOCK": "true",
                 "MAX_TICK_TIME": "60000",
                 "MAX_MEMORY": "1600M"
             },
             logging=ecs.LogDrivers.aws_logs(
-                stream_prefix="minecraft",
+                stream_prefix=name,
                 log_retention=logs.RetentionDays.ONE_WEEK,
             )
         )
-        mc_container.add_port_mappings(ecs.PortMapping(container_port=25565))
-        # mount the EFS volume on the container
-        mc_container.add_mount_points(ecs.MountPoint(
-                                        container_path="/data", 
-                                        source_volume=mc_volume.name, 
-                                        read_only=False)
-        )
+        container.add_port_mappings(ecs.PortMapping(container_port=25565))
+        return container
 
-        # create a service with 0 instances
-        mc_cluster = ecs.Cluster(self, "MinecraftCluster", vpc=mc_vpc)
-        mc_service = ecs.FargateService(self, "minecraft-service", 
-                                    cluster=mc_cluster, 
-                                    task_definition=minecraft,
-                                    assign_public_ip=True,
-                                    desired_count=0,
-                                    security_group=mc_sg,
-                                    platform_version=ecs.FargatePlatformVersion.VERSION1_4
+    #
+    # Create an efs volume to mount on a container
+    #
+    def create_efs_volume(self, name):
+        # create an EFS filesystem and access point
+        fs = efs.FileSystem(self, name + '-fs', 
+                            vpc=self.vpc,
+                            performance_mode=efs.PerformanceMode.GENERAL_PURPOSE,
+                            throughput_mode=efs.ThroughputMode.BURSTING,
+                            security_group=self.efs_sg
         )
+        fs.add_access_point(name, path="/")
+        # define an ECS volume for this filesystem
+        volume = ecs.Volume(
+            name=name + '-volume',
+            efs_volume_configuration=ecs.EfsVolumeConfiguration(
+                file_system_id=fs.file_system_id
+            )
+        )
+        return volume
